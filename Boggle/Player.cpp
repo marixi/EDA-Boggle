86b7b2f315a28a2572dceb50e588d53263f2ac2f
RicardoCQB <ricardo_cqb@hotmail.com>
2019-12-17 18:08:54 +0000

    Updated player.cpp
    Added some functions to the class.

--- Boggle/Player.cpp
+++ Boggle/Player.cpp
@@ -4,3 +4,25 @@
 // Mariana Xavier
 
 #include"Player.h"
+
+Player::Player()
+{
+	_name = "NO NAME";
+	_age = 0;
+}
+
+Player::Player(string name)
+{
+	_name = name;
+	_age = 0;
+}
+void Player::readInfo()
+{
+	cout << "Insert Player's name: "; cin >> _name;
+	cout << "Insert Player's age: "; cin >> _age;
+}
+void Player::readWordsTimed(unsigned int duration)
+{
+
+}
+void display(ostream& os) const;
--- Boggle/Player.h
+++ Boggle/Player.h
@@ -2,18 +2,29 @@
 // Duarte Rodrigues
 // Ricardo Brioso
 // Mariana Xavier
-
 #pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <conio.h>
+
+using namespace std;
 
 class Player
 {
 public:
 	Player(); // builds a player with name="NO NAME"
+	Player(string name);
 	void readInfo(); // reads player's info (name, age)
 	void readWordsTimed(unsigned int duration); // reads words of the player's bet, in one round
 	void display(ostream& os) const;  // showInfo()
 	// TO DO: choose other methods, if necessary
 private:
 	string _name; // name of the player
+	unsigned int _age;
 	// TO DO: other attributes, namely the words of his/her bet (and methods, if necessary)
 };
