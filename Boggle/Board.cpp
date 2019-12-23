095cfc1efda1a0aab71b1175bd39013bc68e8a9a
Duarte-Rodrigues <58471052+Duarte-Rodrigues@users.noreply.github.com>
2019-12-23 22:38:56 +0000

    board almost done
    i changed and almost finalized the board class... i have some doubts about the display of the path

--- Boggle/Board.cpp
+++ Boggle/Board.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 Board::Board(size_t numRows = 4, size_t numCols = 4)
 {
+	_numRows = numRows;
+	_numCols = numCols;
+
 	for (size_t i = 0; i < numRows; i++)
 	{
 		vector<Cube> row;
@@ -78,33 +81,168 @@ char Board::getTopLetter(const Position& pos) const
 //---------------------------------------------------------------------------------------------------------------
 void Board::shuffle()
 {
+	Board fin=Board(_numRows, _numCols);
+	char test,test2;
 	for (size_t row = 0; row < _numRows; row++)
 	{
 		for (size_t col = 0; col < _numCols; col++)
 		{
-			_board[row][col].roll();
+			Cube cube1;
+			int randomRow = rand() % _numRows;
+			int randomCol = rand() % _numRows;
+			Position pos;
+			pos.lin = randomRow;
+			pos.col = randomCol;
+			cube1 = fin.cubeInPosition(pos);
+			test = cube1.getTopLetter();
+			//se no board fin o cubo na posicao random tiver * entao pode-se meter o cubo novo do _board rolar este
+			if (test == '*')
+			{
+				fin.cubeInPosition(pos) = _board[row][col]; //será que substitui? ou é preciso criar uamm função insert?
+			}
+			else //talvez pudesse ser melhorado se os primeiros termos fossem passados por referencia
+			{
+				Position pos1;
+				do{
+					Cube cube2;
+					int randomRow1 = rand() % _numRows;
+					int randomCol1 = rand() % _numRows;
+					
+					pos1.lin = randomRow1;
+					pos1.col = randomCol1;
+					cube2 = fin.cubeInPosition(pos);
+					test2 = cube1.getTopLetter();
+				} while (test2 != '*');
+					fin.cubeInPosition(pos1) = _board[row][col];
+			}
 		}
+
 	}
 
-	// Falta fazer shuffle mas não sei em que isso consiste ainda. 
+	for (size_t row = 0; row < _numRows; row++)
+	{
+		for (size_t col = 0; col < _numCols; col++)
+		{
+			Position finpos;
+			finpos.lin = row;
+			finpos.col = col;
+			_board[row][col] = fin.cubeInPosition(finpos);
+			_board[row][col].roll();
+		}
+	}// para que o valor saia em referencia
+
+	// Falta fazer shuffle mas não sei em que isso consiste ainda. I think its done
 	//Possivelmente, fazer um rand() na posição dos cubos.
 }
 
 //---------------------------------------------------------------------------------------------------------------
 void Board::display(ostream& os) const
 {
+	os << "  1  2  3  4" << "\n";
 	for (size_t row = 0; row < _numRows; row++)
 	{
+		os << row + 1 << " ";
 		for (size_t col = 0; col < _numCols; col++)
 		{
 			_board[row][col].displayTop(os);
+			os << "  ";
 		}
+		os << "\n" << "\n";
 	}
 }
 
 //---------------------------------------------------------------------------------------------------------------
-bool Board::findWord(string word, vector<Position>& path)
+bool Board::findWord(const vector<vector<char>>& board, string word, ostream& os)//(string word, vector<Position>& path) -> argumentos usados inicialmente pelo prof
 {
-	// Meter a função que o prof disponibilizou e perceber como funciona.
+	// Mark all characters as not visited 
+	vector<vector<bool>> visited(_numRows, vector<bool>(_numCols, false));
+
+	// Initialize test word 
+	string str = "";
+
+	//declaration of the path
+	vector<Position> path;
+	
+	// Consider every character and look for 'word', starting with the first character
+	//once that is found pass to findWordAux
+	int numIter = 0;
+	bool found = false;
+	for (int i = 0; i < _numRows; i++)
+		for (int j = 0; j < _numCols; j++)
+			if (board[i][j] == word[0])
+			{
+				Position firstlett;
+				firstlett.lin = i;
+				firstlett.col = j;
+				path.push_back(firstlett);
+
+				findWordAux(board, visited, i, j, str, word, found, os,path);
+			}
+				
+	return found; //depois ao testar dizer que se saiu true entao ta otimo... se saiu false entao deu asneira
 }
 
+//---------------------------------------------------------------------------------------------------------------
+bool Board::findWordAux(const vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j, string& str, const string& word, bool& found, ostream& os, vector<Position>& path)
+{
+	// Mark current board cell as visited and append current character to 'str'
+
+	visited[i][j] = true; //nesta celula sabemos que temos o primeiro char do inicio da palavra
+	str = str + board[i][j]; //str começa vazio
+
+
+	// If 'str' is equal to 'word', then "announce" it has been found
+	if (str == word)
+	{
+		found = true;
+		for (size_t a=0; a < path.size(); a++)
+		{
+			os << "(" << path[a].lin << "," << path[a].col << ") \n";
+		}
+
+	}
+	else
+	{
+		// Visit 8 adjacent cells of board[i][j] 
+		for (int row = i - 1; row <= i + 1 && row < _numRows; row++)
+			for (int col = j - 1; col <= j + 1 && col < _numCols; col++) //o if em baixo é genial
+				if (row >= 0 && col >= 0 && !visited[row][col] && str.length() < word.length() && !found && word.substr(0, str.length()) == str)
+				{
+					Position adduplett;
+					adduplett.lin = row;
+					adduplett.col = col;
+					path.push_back(adduplett);
+					findWordAux(board, visited, row, col, str, word, found, os,path);
+				}
+					
+					//fazer o vetor de positions path com push_backs (found true) e pull_backs (!found)
+		// Erase current character from string, 
+		// remove character position from 'path' and
+		// mark visited[i][j] as false
+		if (!found)
+		{
+			str.erase(str.length() - 1);
+			visited[i][j] = false;
+			path.pop_back();
+		}
+	}
+	return found; 
+	//se uma das letras falha reposta not found e o findwordaux corre todo outra vez,
+	//mas deixando a cellua registada como false, o que nao ativa o if
+}
+
+//---------------------------------------------------------------------------------------------------------------
+Cube Board::cubeInPosition(const Position& pos)
+{
+	return _board[pos.lin][pos.col];
+}
+
+//---------------------------------------------------------------------------------------------------------------
+void Board::displayPath(ostream& os, vector<Position>& path)
+{
+	for (size_t a = 0; a < path.size(); a++)
+	{
+		os << "(" << path[a].lin << "," << path[a].col << ") \n";
+	}
+}
+//NO FINAL, DEPOIS DO DISPLAY, O BOARD É UM vector<vector<char>>, e é nisso que o find word se baseia,i think
--- Boggle/Board.h
+++ Boggle/Board.h
@@ -22,10 +22,13 @@ public:
 	char getTopLetter(const Position& pos) const; // get top letter at position 'pos'
 	void shuffle(); // roll all cubes and shuffle them on the board
 	void display(ostream& os) const; // display board
-	bool findWord(string word, vector<Position>& path); // find 'word' on board
+	bool findWord(const vector<vector<char>>& board, string word, ostream& os);//(string word, vector<Position>& path); 
+	void displayPath(ostream& os, vector<Position>& path);															  // find 'word' on board
 	// if 'word' is found, 'path' must contain the coordinates (lin,col) of the letters of the word
 	// TO DO: choose other methods, if necessary
 private:
+	bool findWordAux(const vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j, string& str, const string& word, bool& found, ostream& os, vector<Position>& path);
+	Cube cubeInPosition(const Position& pos);
 	size_t _numRows, _numCols; // number of columns and number of rows of the board
 	vector<vector<Cube>> _board; // board representation
 	// other attributes and methods
